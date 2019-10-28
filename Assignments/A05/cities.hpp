96ac4fad855c3ee648b17df094e78ccc25e5a586
Stephen Puthenpurackal <sjputhenpurack1225@my.msutexas.edu>
2019-10-28 05:10:17 -0500

    getting stuff started

---
+++ Assignments/A05/cities.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "JsonFacade.hpp"
+#include "Geo.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+class Cities{
+    private:
+    struct City{
+        int Population;
+        string Name;
+        double Longitude;
+        double Latitude;
+        int Rank;
+        string State;
+        double Priority;
+        City(){
+            Population = 0;
+            Name = "";
+            Longitude = 0.0;
+            Latitude = 0.0;
+            Rank = 0.0;
+            State = "";
+            Priority = 0.0;
+        }
+        City(int P,string N, double Long,double Lat, int R, string St, double Pri):
+        Population(P), Name(N), Longitude(Long), Latitude(Lat), Rank(R), State(St), Priority(Pri){}
+        City(json obj){
+            Population = obj["Population"];
+            Name = obj["Name"];
+            Longitude = obj["Longitude"];
+            Latitude = obj["Latitude"];
+            Rank = obj["Rank"];
+            State = obj["State"];
+            Priority = 0.0;
+        }
+    };
+    public:
+
+
+
+};
---
+++ Assignments/A05/graph_builder.cpp
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Date:             28 October 2019
+// Title:            Program 3 - Graphs
+// Semester:         Spring 2018
+// Course:           CMPS 3013 
+// Assignment:       A05
+// Author:           Stephen Puthenpurackal
+// Email:            datfoosteve@gmail.com
+// Files:            (list of source files)
+// Description:
+//       describe program here thoroughly
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "JsonFacade.hpp"
+#include "Geo.hpp"
+#include "cities.hpp"
+#include "Heap.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+
+int main() {
+ofstream outfile;
+outfile.open("output.txt");
+
+string CitiesInput = "cities.json";
+json object;
+JsonFacade JF(CitiesInput);
+
+Cities **City;
+Cities **ReadCity;
+
+int SizeOfJsonFile = JF.getSize();
+Heap<Cities> H(SizeOfJsonFile + 1,false);
+
+
+
+
+
+
+return 0;  
+}
